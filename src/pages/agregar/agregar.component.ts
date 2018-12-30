97269d5c385a5084e6374016cd321db12aceb37f
jesus <imangelo@hotmail.es>
2018-12-30 18:14:15 +0100

    agregar una lista a nuestro servicio

--- src/pages/agregar/agregar.component.ts
+++ src/pages/agregar/agregar.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from '../../models';
-import { NavController } from 'ionic-angular';
+import { NavParams } from 'ionic-angular';
 
 @Component({
-  selector: 'page-agregar',
-  templateUrl: 'agregar.component.html'
+    selector: 'page-agregar',
+    templateUrl: 'agregar.component.html'
 })
 export class AgregarPage {
+    lista: Lista;
 
-  constructor(
-      public deseosService: DeseosService,
-      private navCtrl: NavController) {
-  }
+    constructor(
+        public deseosService: DeseosService,
+        private navParams: NavParams) {
+        const titulo = this.navParams.get('titulo');
+        this.lista = new Lista(titulo);
+    }
 }
--- src/pages/pendientes/pendientes.ts
+++ src/pages/pendientes/pendientes.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { DeseosService } from '../../services/deseos.service';
 import { Lista } from '../../models';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 import { AgregarPage } from '../agregar/agregar.component';
 
 @Component({
@@ -12,7 +12,8 @@ export class PendientesPage {
 
   constructor(
     public deseosService: DeseosService,
-    private navCtrl: NavController) {
+    private navCtrl: NavController,
+    private alertCtrl: AlertController ) {
   
   }
 
@@ -21,6 +22,25 @@ export class PendientesPage {
   }
 
   public agregarLista(): void {
-    this.navCtrl.push(AgregarPage);
+    const alerta = this.alertCtrl.create({
+      title: 'Nueva lista',
+      message: 'Nombre de la nueva lista que desea',
+      inputs: [{
+        name: 'titulo',
+        placeholder: 'Nombre de la lista'
+      }],
+      buttons: [{
+        text: 'Cancelar',
+      },{
+        text: 'Agregar',
+        handler: data => {
+          if (data.titulo.length === 0) {
+            return;
+          }
+          this.navCtrl.push(AgregarPage, {titulo: data.titulo});
+        }
+      }]
+    });
+    alerta.present();
   }
 }
